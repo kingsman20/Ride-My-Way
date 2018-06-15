5de8edc48c1f4f583453880385f23733d83eb564
kingslife20 <kingslifeudo@gmail.com>
2018-06-15 07:52:05 +0100

    [finishes #158359336] Added function to redirect user to dashboard after login

--- UI/js/script.js
+++ UI/js/script.js
@@ -66,3 +66,36 @@ enableLogin = () => {
 signin = () => { 
 	window.location.href = "driver_page.html";
 }
+
+// Driver's Tab on dashboard
+driverMenu = (evt, menu) => {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(menu).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Passenger's Tab on dashboard
+passengerMenu = (evt, menu) => {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(menu).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
