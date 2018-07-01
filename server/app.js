f0b4f10d9d84036e999b15be82bc53cecd7762e6
kingslife20 <kingslifeudo@gmail.com>
2018-07-01 18:47:54 +0100

    Added authentication for ride routes

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 import routes from './routes/routes';
 import userRoute from './routes/userRoute';
-import client from './config/database';
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -15,17 +14,6 @@ const port = parseInt(process.env.PORT, 10) || 3000;
 app.get('/', (req, res) => {
   res.send('Welcome to Ride-My-Way');
 });
-
-// Heroku TEST
-// app.get('/db', async (req, res) => {
-//   try {
-//     const result = await client.query('SELECT * FROM test_table');
-//     res.send({ status: 'success', data: result });
-//     client.release();
-//   } catch (err) {
-//     res.send(`Error = ${err}`);
-//   }
-// });
 
 // User Routes
 app.use(userRoute.usersRoute);
--- server/models/users.js
+++ server/models/users.js
@@ -1,5 +1,1 @@
-import client from '../config/heroku';
 
-client.query('CREATE TABLE users ( id INT PRIMARY KEY, name VARCHAR(20) )', (err, result) => {
-  
-});
