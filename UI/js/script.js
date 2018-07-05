720a7915f82307671ba50eb80022be6f2ca2f11f
kingslife20 <kingslifeudo@gmail.com>
2018-07-05 11:14:21 +0100

    Added script to get rides from heroku

--- UI/js/script.js
+++ UI/js/script.js
@@ -4,7 +4,7 @@ check = (event) => {
 };
 
 getRides = () => {
-  fetch('https://still-basin-40207.herokuapp.com/api/v1/rides?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaWF0IjoxNTMwNjcyOTk3LCJleHAiOjE1MzA3NTkzOTd9.3BoBigJJGqeu3kn6WvXHl3A0xUhMFxNzdejh7vIOM0Y')
+  fetch('https://still-basin-40207.herokuapp.com/api/v1/rides?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTEsIm5hbWUiOiJLaW5nc2xleSBNaWNoZWFsIiwiaWF0IjoxNTMwNzc5MTI0LCJleHAiOjE1MzA4NjU1MjR9.wKwd2jAd0wktlNTyZV76guJDqEyi0o6q9ICCpHproo4')
     .then((res) => res.json())
     .then((data) => console.log(data));
 };
