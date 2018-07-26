810b6c5bb0e916006ef9a4e2748ac724f7c3c44c
kingslife20 <kingslifeudo@gmail.com>
2018-07-26 11:03:12 +0100

    Refactor the test logic

--- UI/js/rides.js
+++ UI/js/rides.js
@@ -1,4 +1,4 @@
-const url = 'http://localhost:3000/api/v1';
+const url = 'https://still-basin-40207.herokuapp.com/api/v1';
 const user = JSON.parse(localStorage.getItem('user'));
 
 const ridesGiven = () => {
